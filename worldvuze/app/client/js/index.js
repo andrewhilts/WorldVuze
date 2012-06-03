43b0f0e4ed103fa43455ed1ad402f3db9359d319
yagnik <yagnikkhanna@gmail.com>
2012-06-03 02:27:25 -0400

    update

--- worldvuze/app/client/js/index.js
+++ worldvuze/app/client/js/index.js
@@ -7,8 +7,28 @@ Template.login.events = {
     var form = $('form[name=login]')[0];
     Meteor.call('sign_up_user', form.username.value, form.password.value, form.type.value);
   }
-}
+};
 
+Template.location.locations = function(){
+  var list_of_locs = [];
+
+  var get_location = function(location){
+   // #add api to get latitude and longitude and return a hash with lat: and lng:
+  };
+
+  for(i=0; i< Teacher.count; i++){
+    list_of_locs.push(get_location(Teacher[i].location));
+  }
+  return list_of_locs;
+};
+
+
+Template.location.build_map = function(lat,lng){
+  //gmap.js library
+  map.addMarker({
+  lat: lat,
+  lng: lng;});
+};
 //////
 ////// Initialization
 //////
